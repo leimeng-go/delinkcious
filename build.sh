a84ea245a59fd7871b05fba22323035b82e91d6f
leimeng <root@DESKTOP-K7K8OIJ.localdomain>
2021-01-10 00:32:23 +0800

    上下文变量

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ TAG="${STABLE_TAG}.${CIRCLE_BUILD_NUM}"
 ROOT_DIR="$(pwd)"
 SVC_DIR="${ROOT_DIR}/svc"
 cd $SVC_DIR
-docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
 for svc in *; do
     cd "${SVC_DIR}/$svc"
     if [[ ! -f Dockerfile ]]; then
